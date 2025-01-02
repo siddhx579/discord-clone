bac38fd7d7bee707032774e1a2fc52fe044568b7
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-01-02 12:05:39 +0530

    adding dark and light theme setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Open_Sans } from 'next/font/google';
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ['latin'] });
 
@@ -29,11 +31,13 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider afterSignOutUrl="/">
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body
-          className={font.className}
+          className={cn(font.className, "bg-white dark:bg-[#313338]")}
         >
-          {children}
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false} storageKey="discord-theme">
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
