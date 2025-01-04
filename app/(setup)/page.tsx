5fd5685f51952c052f85555e7ff463ad50776ead
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-01-04 13:05:27 +0530

    initial modal ui

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { redirect } from "next/navigation";
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
+import { InitialModal } from "@/components/modals/intital-modal";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
@@ -19,9 +20,7 @@ const SetupPage = async () => {
         return redirect(`/servers/${server.id}`);
     }
 
-    return (
-        <div>Create a server</div>
-    )
+    return <InitialModal />
 }
 
 export default SetupPage;
