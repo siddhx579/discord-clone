41f7031e3c96edab807e8380c9c9e5d195590d95
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-01-05 11:26:59 +0530

    complete uploadthing functionality

--- components/modals/intital-modal.tsx
+++ components/modals/intital-modal.tsx
@@ -3,12 +3,13 @@
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { useEffect, useState } from "react";
 
 import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { FileUpload } from "@/components/file-upload";
 
 const formSchema = z.object({
     name: z.string().min(1, {
@@ -59,7 +60,13 @@ export const InitialModal = () => {
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                TODO: Image Upload
+                                <FormField control={form.control} name="imageUrl" render={({ field }) => (
+                                    <FormItem>
+                                        <FormControl>
+                                            <FileUpload endpoint="serverImage" value={field.value} onChange={field.onChange} />
+                                        </FormControl>
+                                    </FormItem>
+                                )} />
                             </div>
                             <FormField control={form.control} name="name" render={({ field }) => (
                                 <FormItem>
