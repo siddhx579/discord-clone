5f0570a971d0d5eb5262a511dc101d9b2be24073
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-01-17 23:20:07 +0530

    Update navigation-sidebar.tsx

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,9 +1,11 @@
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { NavigationAction } from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
+import { ModeToggle } from "@/components/mode-toggle";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationAction } from "./navigation-action";
 import { NavigationItem } from "./navigation-item";
 
 export const NavigationSidebar = async () => {
@@ -34,6 +36,17 @@ export const NavigationSidebar = async () => {
                     </div>
                 ))}
             </ScrollArea>
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                <ModeToggle />
+                <UserButton 
+                    afterSignOutUrl="/" 
+                    appearance={{
+                        elements: {
+                            avatarBox: "h-[48px] w-[48px]"
+                        }
+                    }} 
+                />
+            </div>
         </div>
     )
 };
