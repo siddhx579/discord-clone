4a5751938828465f9b699df073dfa06fcd0b0af6
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-03-01 23:18:12 +0530

    Update initial-modal.tsx

--- components/modals/initial-modal.tsx
+++ components/modals/initial-modal.tsx
@@ -6,8 +6,22 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useEffect, useState } from "react";
 
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "@/components/ui/dialog"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from "@/components/ui/dialog";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
@@ -15,12 +29,12 @@ import { useRouter } from "next/navigation";
 
 const formSchema = z.object({
     name: z.string().min(1, {
-        message: "Server name is required."
+        message: "Server name is required.",
     }),
     imageUrl: z.string().min(1, {
-        message: "Server image is required."
-    })
-})
+        message: "Server image is required.",
+    }),
+});
 
 export const InitialModal = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -36,7 +50,7 @@ export const InitialModal = () => {
         defaultValues: {
             name: "",
             imageUrl: "",
-        }
+        },
     });
 
     const isLoading = form.formState.isSubmitting;
@@ -53,7 +67,7 @@ export const InitialModal = () => {
         }
     };
 
-    if(!isMounted) {
+    if (!isMounted) {
         return null;
     }
 
@@ -65,32 +79,51 @@ export const InitialModal = () => {
                         Customize your server
                     </DialogTitle>
                     <DialogDescription className="text-center text-zinc-500">
-                        Give your server a personality with a name and image. You can always change it later
+                        Give your server a personality with a name and an image. You can
+                        always change it later.
                     </DialogDescription>
                 </DialogHeader>
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                         <div className="space-y-8 px-6">
                             <div className="flex items-center justify-center text-center">
-                                <FormField control={form.control} name="imageUrl" render={({ field }) => (
-                                    <FormItem>
-                                        <FormControl>
-                                            <FileUpload endpoint="serverImage" value={field.value} onChange={field.onChange} />
-                                        </FormControl>
-                                    </FormItem>
-                                )} />
+                                <FormField
+                                    control={form.control}
+                                    name="imageUrl"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <FileUpload
+                                                    endpoint="serverImage"
+                                                    value={field.value}
+                                                    onChange={field.onChange}
+                                                />
+                                            </FormControl>
+                                        </FormItem>
+                                    )}
+                                />
                             </div>
-                            <FormField control={form.control} name="name" render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
-                                        Server name
-                                    </FormLabel>
-                                    <FormControl>
-                                        <Input disabled={isLoading} className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0" placeholder="Enter server name" {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )} />
+
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                                            Server name
+                                        </FormLabel>
+                                        <FormControl>
+                                            <Input
+                                                disabled={isLoading}
+                                                className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                                                placeholder="Enter server name"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
                         </div>
                         <DialogFooter className="bg-gray-100 px-6 py-4">
                             <Button variant="primary" disabled={isLoading}>
@@ -101,5 +134,5 @@ export const InitialModal = () => {
                 </Form>
             </DialogContent>
         </Dialog>
-    )
+    );
 };
